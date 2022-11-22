e3a77ee24463e071d1e8e98d5e1c74c165aeeea5
Akshay <jainakshay058@gmail.com>
2022-11-22 08:14:28 +0530

    Display Posts & Related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,27 @@
+const { populate } = require('../models/post');
+const Post = require('../models/post');
+const { post } = require('../routes');
+
 module.exports.home=function(req,res){
     //return res.end('<h1>Express is up for Codeial</h1>')
-    console.log(req.cookies);
-    return res.render('home',{
-        title: "Home"
+    //console.log(req.cookies);
+
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title: "Codeial|Home" ,
+    //         posts: posts
+
+    // });
+    // });
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+        title: "Codeial|Home" ,
+        posts: posts 
+        });
     });
+
+
+    
 }
