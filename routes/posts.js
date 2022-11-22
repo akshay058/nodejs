20dadda1ec747652faedd25c7382698f3624a158
Akshay <jainakshay058@gmail.com>
2022-11-22 14:31:55 +0530

    Adding Comments To DB

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const passport = require('passport');
 const router =express.Router();
-
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 router.post('/create',passport.checkAuthentication,postsController.create);
