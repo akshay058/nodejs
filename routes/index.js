9f9898f0afc0a0af8a3d27a90056487ed5c148c8
Akshay <jainakshay058@gmail.com>
2022-11-22 07:12:27 +0530

    saving post to db

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/',homeController.home);
 //for any further routes access from here
 //router.use('/routerName',require('./routerfile'));
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 
 router.use('/api',require('./api'));
 
