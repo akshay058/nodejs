9063c7758818c052e5f2a3734574a0dcb2884a19
Akshay <jainakshay058@gmail.com>
2022-11-25 18:42:18 +0530

    Converting Controller CODE to ASYNC

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,45 +1,100 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
+// Create comment....................................................
 
-module.exports.create = function(req,res){
-    Post.findById(req.body.post,function(err,post){
+
+module.exports.create = async function(req,res){
+
+    try{
+
+        let post = await Post.findById(req.body.post);
+
         if(post){
-            Comment.create({
+           let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            },function(err,comment){
-                if(err){ console.log("Error in finding comments");return;}
+            });
             post.comments.push(comment); // add comments to post model db when comments created
             post.save(); // always do save after adding
+            res.redirect('/');
+        }   
+    }
 
-            return res.redirect('/');
-
-            });
-        }
-    });
-
+    catch(err){
+        console.log("Error",err);
+        return;
+    }
 }
 
-// Delete comments
+// module.exports.create = function(req,res){
+//     Post.findById(req.body.post,function(err,post){
+//         if(post){
+//             Comment.create({
+//                 content: req.body.content,
+//                 post: req.body.post,
+//                 user: req.user._id
+//             },function(err,comment){
+//                 if(err){ console.log("Error in finding comments");return;}
+//             post.comments.push(comment); // add comments to post model db when comments created
+//             post.save(); // always do save after adding
 
-module.exports.destroy = function(req,res){
-    Comment.findById(req.params.id,function(err,comment)
-    {
+//             return res.redirect('/');
+
+//             });
+//         }
+//     });
+
+// }
+
+// Delete comments.............................................
+
+
+
+module.exports.destroy = async function(req,res){
+
+    try{
+        let comment = await Comment.findById(req.params.id);
+
         //.id means converting the object id into string
         if(comment.user==req.user.id){
-
             let postId =comment.post;
-
             comment.remove();
 
-            Post.findByIdAndUpdate(postId,{ $pull: {comments:req.params.id}},function(err,post){
-                return res.redirect('back');
-            });
+            let post = Post.findByIdAndUpdate(postId,{ $pull: {comments:req.params.id}});
+            return res.redirect('back');
         }
         else{
             return res.redirect('back');
         }
-    });
+
+    }
+
+    catch(err)
+    {
+        console.log("Error",err);
+        return;
+    }
+   
 }
+
+// module.exports.destroy = function(req,res){
+//     Comment.findById(req.params.id,function(err,comment)
+//     {
+//         //.id means converting the object id into string
+//         if(comment.user==req.user.id){
+
+//             let postId =comment.post;
+
+//             comment.remove();
+
+//             Post.findByIdAndUpdate(postId,{ $pull: {comments:req.params.id}},function(err,post){
+//                 return res.redirect('back');
+//             });
+//         }
+//         else{
+//             return res.redirect('back');
+//         }
+//     });
+// }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,41 +3,59 @@ const Post = require('../models/post');
 const { post } = require('../routes');
 const User = require('../models/user');
 
+module.exports.home= async function(req,res){
 
-module.exports.home=function(req,res){
-    //return res.end('<h1>Express is up for Codeial</h1>')
-    //console.log(req.cookies);
-
-
-    // Post.find({},function(err,posts){
-    //     return res.render('home',{
-    //         title: "Codeial|Home" ,
-    //         posts: posts
-
-    // });
-    // });
-    // populate the user for each post
-    Post.find({})
-    .populate('user')
-    .populate({
+    try{
+        //populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
         path:'comments',
         populate: {
             path: 'user'
-        }
-    }).exec(function(err,posts){
-
-        // finding all users
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: "Codeial|Home" ,
-                posts: posts,
-                all_users: users 
-
+            }
         });
+
+        let users = await User.find({});
+        return res.render('home',{
+        title: "Codeial|Home" ,
+        posts: posts,
+        all_users: users 
+        });
+
+    }
+    catch(err){
+        console.log("Error",err);
+        return;
+    }
+}
+
+
+
+// module.exports.home=function(req,res){
+//     //console.log(req.cookies);
+//     // populate the user for each post
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path:'comments',
+//         populate: {
+//             path: 'user'
+//         }
+//     }).exec(function(err,posts){
+
+//         // finding all users
+//         User.find({},function(err,users){
+//             return res.render('home',{
+//                 title: "Codeial|Home" ,
+//                 posts: posts,
+//                 all_users: users 
+
+//         });
         
-        });
-    });
+//         });
+//     });
 
 
     
-}
+// }
