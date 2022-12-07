dd3121c40acc944a290e416eb68972e70eae09ed
Akshay <jainakshay058@gmail.com>
2022-12-07 20:48:38 +0530

    add multer file upload config  to user

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const multer = require('multer'); // for file uploads
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars'); // path given for saving files 
 
 const UserSchema = new mongoose.Schema({
     email: {
